49990c083220ddcf46942cd2d2c3d3a1073722ff
kleber <catce.2020111mtds0441@aluno.ifpi.edu.br>
2022-10-13 19:24:24 -0300

    projeto criado

---
+++ app.py
@@ -0,0 +1,8 @@
+def main():
+    print('Hello, world')
+
+def outraFuncao():
+    print('outra funcao executada')
+    
+if __name__ =="__main__":
+    main()
---
+++ usuario.py
@@ -0,0 +1,9 @@
+class Usuario:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+    
+    
+    
+user = Usuario(username='Andrews', email='catce.2020111mtds0441@aluno.ifpi.edu.br', password=0234)
